83f43fb8316ae07d0e52c2a5fc559c0091f5a98d
Mahdia50 <106918349+Mahdia50@users.noreply.github.com>
2024-05-21 00:36:32 +0100

    Delete plugins directory

--- plugins/فكك حل.js
+++
@@ -1,8 +0,0 @@
-const answers = {
-    "1": "لوفي",
-    "2": "ناروتو",
-    "3": "جوجو",
-    // وهكذا
-}
-
-export default answers
--- plugins/لقبني.js
+++
@@ -1,8 +0,0 @@
-> استيراد { createHash } من 'crypto' Let Reg = /|?(.*)( _?)([0-9]_ )$/i Let Handler = async function (m, { conn, text,usedPrefix, Command }) { السماح للمستخدم = global.db.data.users[m.sender] السماح name2 = conn.getName(m.sender) إذا (user.registered === صحيح) رمي `✳️ أنت ملقب بالفعل يا حب` إذا (!Reg.test(text)) رمي `⚠️ تنسيق غير صحيح\n\n✳️ استخدم هذا الأمر: *${usedPrefix + command} القب*\n📌 مثال: *${usedPrefix + command}* لقبك واسم النقابه\n📌 مثال: *${usedPrefix + command}* كيلوا كلاود` السماح [ _, name] = text.match(Reg) if (!name) throw ' _تنسيق غير صحيح المثال: .لقبني كيلوا كلود_ ' if (name.length >= 30) throw '✳️ يسحب طويل جداً' user.name = name. تقليم () user.regTime = + تاريخ جديد user.registered = true Let sn = createHash('md5').update(m.sender).digest('hex') m.reply( ` ┌─「 *ملقب* 」─ ▢ *القب:* ${name} ▢ *الرقم* : ${m.sender.split`@`[0]} └─ ─────────────
-> 
-> _${usedPrefix}اوامر_ تقديم القائمة `.trim()) }
-> 
-> Handler.help = ['لقبني'].map(v => v + ' <الاسم>') Handler.tags = ['rg'] Handler.command = ['لقبني']
-> 
-> معالج التصدير الافتراضي
-
